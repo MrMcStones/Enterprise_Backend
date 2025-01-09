b22a948430aee155b70558fc1d26ec4fa7d82fa8
MrMcStones <rasmus.ke.nilsson@gmail.com>
2025-01-09 03:41:37 +0100

    Added and fixed code for classes

--- backend/src/main/java/com/rasmus/enterprise_project/backend/repository/UserRepository.java
+++ backend/src/main/java/com/rasmus/enterprise_project/backend/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.rasmus.enterprise_project.backend.repository;
 
-public class UserRepository {
+
+import com.rasmus.enterprise_project.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
--- backend/src/main/java/com/rasmus/enterprise_project/backend/service/UserService.java
+++ backend/src/main/java/com/rasmus/enterprise_project/backend/service/UserService.java
@@ -1,4 +1,25 @@
 package com.rasmus.enterprise_project.backend.service;
 
+
+import com.rasmus.enterprise_project.backend.model.User;
+import com.rasmus.enterprise_project.backend.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User registerUser(String username, String rawPassword) {
+        String hashedPassword = passwordEncoder.encode(rawPassword);
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(hashedPassword);
+        return userRepository.save(user);
+    }
 }
